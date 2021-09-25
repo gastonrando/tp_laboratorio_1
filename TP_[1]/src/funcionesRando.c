d0c003c6293204f716a6c8122258a43f4c6daffe
Gaston Rando <gastonrando@gmail.com>
2021-09-25 15:15:43 -0300

    Version 2.0.2

--- TP_[1]/src/funcionesRando.c
+++ TP_[1]/src/funcionesRando.c
@@ -9,9 +9,6 @@
 #include <stdlib.h>
 #include "funcionesRando.h"
 
-/// @param mensaje mensaje asignado en el main e impreso en el printf de la funcion
-///se lee el valor int ingresado por el usuario
-/// @return devuelve al main el numero ingresado por el usuario
 int ingresarEntero( char mensaje[]){
     int numeroIngresado;
 
@@ -21,15 +18,6 @@ int ingresarEntero( char mensaje[]){
     return numeroIngresado;
 }
 
-
-/// @param flagNum1 trae del main la bandera de la carga del primer operador
-/// @param flagNum2 trae del main la bandera de la carga del segundo operador
-/// @param num1 trae el operador para exponer como x
-/// @param num2 trae el operador para exponer como y
-/// imprime las opciones que el usuario puede ingresar
-/// el usuario no puede ingresar la opcion 3 sin haber ingresado la 1 y la 2
-/// tampoco puede ingresar la 4 sin haber realizado la 3 y/o 1 y 2
-/// @return devuelve la opcion seleccionada
 int menu(int flagNum1, int flagNum2,int num1, int num2){
 
     int opcion;
@@ -62,20 +50,6 @@ int menu(int flagNum1, int flagNum2,int num1, int num2){
    return opcion;
 }
 
-
-/*int ingresoNum(){
-    int num;
-
-    printf("Ingrese un operador \n");
-    scanf("%d", &num);
-
-    return num;
-}*/
-
-/// @param num1 trae del main el primer operador
-/// @param num2 trae del main el segundo operador
-/// se suman los operadores
-/// @return devuelve la suma
 int sumaNumeros(int num1, int num2){
 
 	int resultadoSuma;
@@ -85,10 +59,6 @@ int sumaNumeros(int num1, int num2){
     return resultadoSuma;
 }
 
-/// @param num1 trae del main el primer operador
-/// @param num2 trae del main el segundo operador
-/// realiza la resta
-/// @return devuelve el resultado
 int restaNumeros(int num1, int num2){
 
 	int resultadoResta;
@@ -98,12 +68,6 @@ int restaNumeros(int num1, int num2){
 	return resultadoResta;
 }
 
-/// @param num1 trae del main el primer operador
-/// @param num2 trae del main el segundo operador
-/// si el segundo operador es = 0 se registra que existe error = 1
-/// caso contrario no hay error y se calcula la division
-/// @param pResultadoDivision se guarda el valor de la variable puntero la cual tendra el resultado
-/// @return se devuelve si hay error, si es posible realizar la division
 int divisionNumeros(int num1, int num2, float* pResultadoDivision){
 
     int error;
@@ -118,10 +82,6 @@ int divisionNumeros(int num1, int num2, float* pResultadoDivision){
     return error;
 }
 
-/// @param num1 trae del main el primer operador
-/// @param num2 trae del main el segundo operador
-/// se realiza la multiplicacion
-/// @return devuelve el resultado de la multiplicacion
 int multiplicarNumeros (int num1, int num2){
 
 	int resultadoMultiplicar;
@@ -132,10 +92,6 @@ int multiplicarNumeros (int num1, int num2){
 
 }
 
-/// @param num trae el operador sobre el cual se calculara el factorial
-/// la funcion es iterativa, si num es 1 o 0 devolvera 1, si es negativo devolvera 0 implicando falsedad y que no tiene factorial
-/// si es mayor a 1 la funcion iterara hasta que el numero sea igual a 1 y terminara la iteracion
-/// @return
 long long int factorial (int num){
 
     int retorno;
--- TP_[1]/src/funcionesRando.h
+++ TP_[1]/src/funcionesRando.h
@@ -8,13 +8,70 @@
 #ifndef FUNCIONESRANDO_H_
 #define FUNCIONESRANDO_H_
 
+/**
+ *
+ * @param mensaje mensaje asignado en el main e impreso en el printf de la funcion
+ * @brief se lee el valor int ingresado por el usuario y lo graba en una variable
+ * @return devuelve al main el numero ingresado por el usuario
+ */
 int ingresarEntero( char mensaje[]);
-int ingresoNum();
+
+/**
+ *
+ * @param flagNum1 bandera que controla el ingreso del primer operador
+ * @param flagNum2 bandera que controla el ingreso del segundo operador
+ * @param num1 trae el operador 1 de la funcion main
+ * @param num2 trae el operador 2 de la funcion main
+ * @brief la funcion imprime el menu y controla que se hayan ingresado los operadores para mostrarlos tambien en el menu,
+ * luego devuelve al main la opcion seleccionada como un int
+ * @return
+ */
 int menu(int flagNum1, int flagNum2,int num1,int num2);
+
+/**
+ *
+ * @param num1 trae del main el primer operador
+ * @param num2 trae del main el segundo operador
+ * @brief se suman los operadores num1, num2
+ * @return devuelve la suma
+ */
 int sumaNumeros(int num1, int num2);
+
+/**
+ *
+ * @param num1 trae el operador 1 de la funcion main
+ * @param num2 trae el operador 2 de la funcion main
+ * @brief se restan los operadores num1, num2
+ * @return devuelve el resultado de la resta
+ */
 int restaNumeros(int num1, int num2);
+
+/**
+ *
+ * @param num1 trae el operador 1 de la funcion main
+ * @param num2 trae el operador 2 de la funcion main
+ * @param pResultadoDivision se guarda el valor del puntero en una direccion de memoria determinada
+ * @brief la funcion analisa que el segundo operador sea distinto de 0. Si es igual a 0 el error es verdadero y no es falso
+ * @return devuelve el valor de error para ser analizado en el main
+ */
 int divisionNumeros(int num1, int num2, float* pResultadoDivision);
+
+/**
+ *
+ * @param num1 trae el operador 1 de la funcion main
+ * @param num2 trae el operador 2 de la funcion main
+ * @brief la funcion multiplica el num1 y num2
+ * @return devuelve el resultado de la aplicacion
+ */
 int multiplicarNumeros (int num1, int num2);
+
+/**
+ *
+ * @param num recibe el valor de la variable que haya enviado desde el main
+ * @brief la funcion analisa si el valor que trae es negativo (no posee factorial), 0 y 1 factorial =1 y
+ * si es mayor a 1 itera el factorial en la misma funcion hasta que cumpla que sea igual a 1
+ * @return devuelve el resultado del factorial
+ */
 long long int factorial (int num);
 
 
